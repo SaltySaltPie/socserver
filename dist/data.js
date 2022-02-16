25a705651f9ff0dfd45e54981f94211080a3382a
SaltySaltPie <84831252+SaltySaltPie@users.noreply.github.com>
2022-02-16 22:31:59 +0700

    remove dist

--- dist/data.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.clientURL = void 0;
-exports.clientURL = 'http://localhost:3000/';
-//# sourceMappingURL=data.js.map
